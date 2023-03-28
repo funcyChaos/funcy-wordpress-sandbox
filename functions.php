e153bf33b41b9c1f68474cd9866a8d1a056cf63f
Austin Reilly <areilly@bitwiseindustries.com>
2023-03-28 13:08:49 -0700

    refactor(functions): add second endpoint part thing

--- functions.php
+++ functions.php
@@ -21,11 +21,11 @@ add_action('wp_ajax_nopriv_admin-ajax', function(){
 
 /**
  * Add REST API endpoint. Supposed to be faster than admin-ajax, but the last known benchmark, at time of writing, says it is actually slower
- * This route is available at /wp-json/api-endpoint and /wp-json/api-endpoint/endpoint/{param}
+ * This route is available at /wp-json/api-endpoint/v1 and /wp-json/api-endpoint/v1/endpoint/{param}
  */
 
 add_action('rest_api_init', function(){
-	register_rest_route('api-endpoint', '/endpoint/(?P<param>\d+)', [
+	register_rest_route('api-endpoint/v1', '/endpoint/(?P<param>\d+)', [
 		[
 			'methods'	=> 'GET',
 			'callback'	=> function (WP_REST_Request $req){
