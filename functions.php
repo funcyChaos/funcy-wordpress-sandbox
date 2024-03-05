aa6fabe08907285a6728c531c4e90212d9564533
Austin Reilly <austinreilly51@gmail.com>
2024-03-04 21:31:29 -0800

    feat(curl): stronger way of calling api ig

--- functions.php
+++ functions.php
@@ -25,3 +25,24 @@ add_action('rest_api_init', function(){
 		],
 	]);
 });
+
+// Saving this for later
+
+add_action('save_post', function($post_id){
+	if(get_post_type($post_id) == 'porch'){
+		$key = map_api_key;
+		$address = urlencode(get_field('porch_address', $post_id));
+		$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key={$key}";
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($curl);
+		curl_close($curl);
+	
+		$data = json_decode($response, true);
+		$latitude = $data['results'][0]['geometry']['location']['lat'];
+		$longitude = $data['results'][0]['geometry']['location']['lng'];
+		update_field('latitude', $latitude, $post_id);
+		update_field('longitude', $longitude, $post_id);
+	}
+}, 10, 1);
